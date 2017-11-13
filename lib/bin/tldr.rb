82c2bc317be115146f2b4d2e7a7675f5ad268cc8
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-13 22:27:49 +0100

    Added help and list options

--- lib/bin/tldr.rb
+++ lib/bin/tldr.rb
@@ -1,18 +1,30 @@
 #!/bin/env ruby
 
+#Checking platform
 unless /linux/ =~ RUBY_PLATFORM then
   puts "Only Linux is supported currently"
   exit
 end
 
 #Gems
+require 'optparse'
 require 'colorize'
+
+#Predefined variables
+options = {}
+@optparse = false
 
 #Pages
 parent_directory = File.expand_path('..', File.dirname(__FILE__))
 @lcpages = "#{parent_directory}/pages/commonlinux"
 
 #Option Parser
+OptionParser.new do |opt|
+  opt.on("--help", "-h") { puts "To display a tldr page, write 'tldr' followed by the command you want to have explained. To get a list of the available commands, you can run 'tldr --list'. I would recommend using it in conjuction with a unix pipeline like so: 'tldr --list | sort | less'\n" ; @optparse = true}
+  opt.on("--list", "-l") { puts Dir.entries(@lcpages) ; @optparse = true }
+end.parse!
+
+#Getting user input and displaying / formatting output
 begin
   Dir.entries(@lcpages).each do |page|
     if page == ARGV[0] + ".md" then
@@ -25,5 +37,5 @@ begin
     end
   end
 rescue NoMethodError
-  puts "You need to append an argument to the file [e.g. ./tldr.rb tree] to display the tldr"
+  puts "ERROR:".colorize(:background => :red) + " You need to append an argument to the file [e.g. ./tldr.rb tree] to display the tldr" unless @optparse
 end
