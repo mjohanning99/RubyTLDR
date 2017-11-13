4956091be251e17dd7d05ddb6bf9999cf2ccdfe1
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-13 17:43:59 +0100

    Restructuring, added common tldr files, added colours

--- lib/bin/tldr.rb
+++ lib/bin/tldr.rb
@@ -1,17 +1,21 @@
 #!/bin/env ruby
 
 #Gems
+require 'colorize'
 require 'redcarpet'
 require 'redcarpet/render_strip'
 
 #Pages
-@lipages = Dir.entries("./lib/pages/linux")
+@lcpages = "./lib/pages/commonlinux"
 
 #Option Parser
-@lipages.each do |page|
+Dir.entries(@lcpages).each do |page|
   if page == ARGV[0] + ".md" then
-    File.open("./lib/pages/linux/#{page}").each_line do |line|
-      puts line
+    File.open("#{@lcpages}/#{page}").each_line do |line|
+      puts line.colorize(:color => :black, :background => :red) if /^#/ =~ line
+      puts line.colorize(:yellow) if /^>/ =~ line
+      puts line.colorize(:green) if /^-/ =~ line
+      puts line if /^`/ =~ line
     end
   end
 end
