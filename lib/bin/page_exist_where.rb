2cab78be591048e8792782526d1aeda70eb70ab5
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-15 12:09:20 +0100

    More restructuring

---
+++ lib/bin/page_exist_where.rb
@@ -0,0 +1,10 @@
+#!/bin/env ruby
+#Checks where the page exists in the page folders
+
+def page_exist_where()
+  return "linux" if Dir.entries(@linux).include?("#{ARGV[0]}.md")
+  return "common" if Dir.entries(@common).include?("#{ARGV[0]}.md")
+  return "windows" if Dir.entries(@windows).include?("#{ARGV[0]}.md")
+  return "osx" if Dir.entries(@osx).include?("#{ARGV[0]}.md")
+  return "sunos" if Dir.entries(@sunos).include?("#{ARGV[0]}.md")
+end
--- lib/bin/tldr.rb
+++ lib/bin/tldr.rb
@@ -20,6 +20,7 @@ end
 require_relative 'download_new_pages.rb'
 require_relative 'check_parse_download.rb'
 require_relative 'parse_md.rb'
+require_relative 'page_exist_where.rb'
 
 #Predefined variables
 options = {}
@@ -27,14 +28,19 @@ options = {}
 @parent_directory = File.expand_path('..', File.dirname(__FILE__))
 
 #Pages
-@lcpages = "#{@parent_directory}/pages/commonlinux"
+@linux = "#{@parent_directory}/pages/linux"
+@common = "#{@parent_directory}/pages/common"
+@windows = "#{@parent_directory}/pages/windows"
+@sunos = "#{@parent_directory}/pages/sunos"
+@osx = "#{@parent_directory}/pages/osx"
 
 #Option Parser
 OptionParser.new do |opt|
   opt.on("--help", "-h") { puts "HELP:".colorize(:background => :green) + " To display a tldr page, write 'tldr' followed by the command you want to have explained.\nWords surrounded by '<>' are files / commands the user needs to add themselves.\nTo get a list of the available commands, you can run 'tldr --list'. I would recommend using it in conjuction with a unix pipeline like so: 'tldr --list | sort | less'\nTo update the list of pages, you can run tldr --update\nTo get even more information about this program, please refer to the README file located in #{File.expand_path('../..', File.dirname(__FILE__))}" ; @optparse = true}
-  opt.on("--list", "-l") { puts Dir.entries(@lcpages) ; @optparse = true }
+  opt.on("--list", "-l") { puts Dir.entries(@linux) ; @optparse = true }
   opt.on("--update", "-u") { `bash #{@parent_directory}/bin/update_pages.sh` ; exit }
 end.parse!
 
 #Getting user input and displaying / formatting output
-check_parse_download(@lcpages)
+check_parse_download(@linux)
+puts page_exist_where
