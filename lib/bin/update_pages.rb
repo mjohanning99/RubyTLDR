d7e13c18e046be1becda423798f52bf7d8a3efe1
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-17 20:13:04 +0100

    Added download for zip file

--- lib/bin/update_pages.rb
+++ lib/bin/update_pages.rb
@@ -1,6 +1,12 @@
 #!/bin/env ruby
 
-#This is the method that will update the page cache so that the pages are up-to date [.zip file, checking the Github folders or downloading the .json file]
+#This is the method that will update the page cache so that the pages are up-to date [.zip file, checking the Github folders or downloading the .json file
+require 'open-uri'
 def update_pages()
-  #TODO
+  File.open("#{File.expand_path('..', File.dirname(__FILE__))}/pages.zip", "wb") do |saved_file|
+    open("https://tldr.sh/assets/tldr.zip", "r") do |read_file|
+      saved_file.write(read_file.read)
+    end
+  end
 end
+update_pages()
