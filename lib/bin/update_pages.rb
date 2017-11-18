ba1a7e47845be6f4557b8fedd4de03fffff5a952
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-18 16:50:26 +0100

    Add downloading of json before downloading zip

--- lib/bin/update_pages.rb
+++ lib/bin/update_pages.rb
@@ -3,19 +3,33 @@
 
 #This is the method that will update the page cache so that the pages are up-to date [.zip file, checking the Github folders or downloading the .json file
 def update_pages()
+  #Check for internet connection (and connection to tldr.sh for assets)
   begin
     TCPSocket.new 'tldr.sh', 443
   rescue SocketError
     puts "ERROR".colorize(:background => :red) + " An error occurred whilst updating the page cache. Please check your internet connection [SocketError]"
     exit
   end
-  #https://tldr.sh/assets/index.json – TODO Download json file first and check if command exists, then download and extract zip
-  FileUtils.rm_rf("#{@parent_directory}/pages")
-  File.open("#{@parent_directory}/pages.zip", "wb") do |saved_file|
-    open("https://tldr.sh/assets/tldr.zip", "r") do |read_file|
+
+  #Downloading index.json from tldr.sh and checking if the entered command exists
+  File.open("#{@parent_directory}/index.json", "wb") do |saved_file|
+    open("https://tldr.sh/assets/index.json", "r") do |read_file|
       saved_file.write(read_file.read)
     end
   end
-  extract_zip("#{@parent_directory}/pages.zip", "#{@parent_directory}")
-  FileUtils.rm("#{@parent_directory}/pages.zip")
+  index = File.read("#{@parent_directory}/index.json")
+  if index.include?(ARGV[0]) then
+    FileUtils.rm_rf("#{@parent_directory}/pages")
+    File.open("#{@parent_directory}/pages.zip", "wb") do |saved_file|
+      open("https://tldr.sh/assets/tldr.zip", "r") do |read_file|
+        saved_file.write(read_file.read)
+      end
+    end
+    extract_zip("#{@parent_directory}/pages.zip", "#{@parent_directory}")
+    FileUtils.rm("#{@parent_directory}/pages.zip")
+  else
+    puts "ERROR:".colorize(:background => :red) + " Page could not be found in database – Not updating cache"
+    FileUtils.rm_rf("#{@parent_directory}/index.json")
+    exit
+  end
 end
