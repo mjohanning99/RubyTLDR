26d0b915e64c7173b8169f9a5cc7e1c967773e42
Marvin Johanning <manimusmuellus@googlemail.com>
2020-02-04 11:34:02 +0100

    Create new installer, change tldr executable file to work with new installer

---
+++ installer.sh
@@ -0,0 +1,39 @@
+##!/bin/bash
+
+if [ -e ~/.rtldr ]; then echo "The program seems to have already been installed since the ~/.rtldr directory already exists. In that case, please use the command 'updatetldr'" && exit; else echo "Installation starting."; fi
+
+if [ -f /bin/rtldr ]
+then
+    echo "It seems like this program has already been installed previously, since there already exists a symlink called 'rtldr' within the /bin folder. It is recommended to delete this file as it may point to an incorrect location and could thus potentially destroy your rtldr installation."
+
+    echo "Would you like for the installer to delete the symbolic link? [1 = Yes, 2 = No]"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) sudo rm /bin/rtldr; break;;
+            No ) echo "The symbolic link will not be removed. Please be aware that this may break your rtldr installation.";;
+        esac
+    done
+
+    echo "Cloning from repository starting."
+    git clone https://gitlab.com/mjohanning/RubyTldr.git ~/.rtldr
+
+    if [ $? == 0 ]; then echo "Cloning successful."; else echo "Cloning failed. Please check your internet connection and verify that you have 'git' installed on your machine." && exit; fi
+
+    echo "Creating symlink of ~/.rtldr in /bin."
+    sudo ln -s ~/.rtldr/tldr /bin/rtldr
+
+    echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'tldr tldr'. If you get an explanation of this program, then everything worked correctly"
+    echo "To update tldr, please run updatetldr"
+
+else
+    echo "Cloning from repository starting."
+    git clone https://gitlab.com/mjohanning/RubyTldr.git ~/.rtldr
+
+    if [ $? == 0 ]; then echo "Cloning successful."; else echo "Cloning failed. Please check your internet connection and verify that you have 'git' installed on your machine." && exit; fi
+
+    echo "Creating symlink of ~/.rtldr in /bin."
+    sudo ln -s ~/.rtldr/tldr /bin/rtldr
+
+    echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'tldr tldr'. If you get an explanation of this program, then everything worked correctly"
+    #echo "To update tldr, please run updatetldr"
+fi
