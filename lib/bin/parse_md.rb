46da106d4119201bff301107a0c33a30a16c018a
Marvin Johanning <manimusmuellus@googlemail.com>
2020-02-05 15:27:56 +0100

    Add license notices into file headers

--- lib/bin/parse_md.rb
+++ lib/bin/parse_md.rb
@@ -1,5 +1,23 @@
 #!/bin/env ruby
 
+=begin
+Copyright 2017-2020 Marvin Johanning
+This file is part of RubyTldr (rtldr).
+
+Reeder is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reeder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Reeder.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 #Parse given markdown file
 def parse_md(directory, page)
   puts "#{operating_system.upcase}".colorize(:background => :red) unless operating_system == "common"
--- lib/bin/update_pages.rb
+++ lib/bin/update_pages.rb
@@ -1,6 +1,24 @@
 #!/bin/env ruby
 # coding: utf-8
 
+=begin
+Copyright 2017-2020 Marvin Johanning
+This file is part of RubyTldr (rtldr).
+
+Reeder is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reeder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Reeder.  If not, see <http://www.gnu.org/licenses/>.
+=end
+
 #This is the method that will update the page cache so that the pages are up-to date [.zip file, checking the Github folders or downloading the .json file
 def update_pages()
   #Check for internet connection (and connection to tldr.sh for assets)
