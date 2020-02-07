39b3e61fe63dcf3ee9d1ef4ae63141b633c5d093
Marvin Johanning <manimusmuellus@googlemail.com>
2020-02-07 20:32:51 +0100

    Remove unecessary folder check, add comments

--- installer.sh
+++ installer.sh
@@ -9,22 +9,25 @@ error_catch() {
 #Variable that stores the ruby version installed on the computer
 ruby_version=$(ruby -v)
 
+#Informing the user about their current Ruby installation / Informing them of a missing Ruby installation and exiting the installer.
 if [[ $ruby_version == *ruby* ]]; then printf "A Ruby installation ($ruby_version) has been found on your machine. Proceeding ... \n\n"; else "No Ruby installation could be found on your machine. Please download the latest version of Ruby using your distribution's package manager or by compiling it from source"; exit; fi
 
+#Check if the default installation directory (~/.rtldr) already exists, inform the user and give them the choice of having it deleted.
 if [ -e ~/.rtldr ]
 then
   echo "The program seems to have already been installed previously, since the ~/.rtldr folder already exists. Do you wish to remove the folder and replace the old RubyTldr version with the one you have just downloaded?"
 
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) echo "Removing old ~./rtldr... " ; rm -r --interactive=never ~/.rtldr ; break;;
-            No ) echo "The symbolic link will not be removed. Please be aware that this may break your rtldr installation. Setting a symlink manually, instead, and potentially altering a few of the files will now be necessary."; break;;
+            Yes ) echo "Removing old ~/.rtldr... " ; rm -r --interactive=never ~/.rtldr ; break;;
+            No ) echo "The old ~/.rtldr directory will not be removed, installation cannot carry on. Aborting." ; exit; break;;
         esac
     done
+  else
+    echo "Installation starting ..."
 fi
 
-if [ -e ~/.rtldr ]; then echo "The program seems to have already been installed since the ~/.rtldr directory already exists. In that case, please use the command 'updatetldr'" && exit; else echo "Installation starting."; fi
-
+#Check whether or not the tldr executable and the tldr.rb file exist and if they do, do not download a new
 if [[ -e tldr && -e lib/bin/tldr.rb ]]
 then
     echo "You seem to have already downloaded the entire repository. Skipping the download ..."
