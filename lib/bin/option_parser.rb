289e597bb1b0ca66a557dd606039053450b9f211
Marvin Johanning <manimusmuellus@googlemail.com>
2020-02-10 15:51:31 +0100

    Update --help flag

--- lib/bin/option_parser.rb
+++ lib/bin/option_parser.rb
@@ -19,14 +19,21 @@ along with RubyTldr.  If not, see <http://www.gnu.org/licenses/>.
 =end
 
 #The option parser that parses the "--help", "--list" and "--update" options, most of which don't work anymore after the restructuring.
-#TODO Rewrite option parser using ARGV[0] instead
 
 #Require update_pages.rb so that --update can be used
 require_relative 'update_pages.rb'
 
 parent_directory = File.expand_path('..', File.dirname(__FILE__))
 OptionParser.new do |opt|
-  opt.on("--help", "-h") { puts "HELP:".colorize(:color => :black, :background => :green) + " To display a tldr page, write 'tldr' followed by the command you want to have explained.\nWords surrounded by '<>' are files / commands the user needs to add themselves.\nTo get a list of the available commands, you can run 'tldr --list'. I would recommend using it in conjuction with a unix pipeline like so: 'tldr --list | sort | less'\nTo update the list of pages, you can run tldr --update\nTo get even more information about this program, please refer to the README file located in #{File.expand_path('../..', File.dirname(__FILE__))}" ; exit }
-  opt.on("--list", "-l") { puts Dir.entries("#{parent_directory}/pages/linux") ; puts Dir.entries("#{parent_directory}/pages/common") ; puts Dir.entries("#{parent_directory}/pages/windows") ; puts Dir.entries("#{parent_directory}/pages/osx") ; puts Dir.entries("#{parent_directory}/pages/sunos") ; exit }
-  opt.on("--update", "-u") { update_pages_opt(parent_directory) ; exit}
+
+  opt.banner = "Usage: rtldr [options]"
+
+  opt.on("--help", "-h", "Prints this help") do
+    puts opt
+    exit
+  end
+
+  opt.on("--list", "-l", "Lists all the available commands") { puts Dir.entries("#{parent_directory}/pages/linux") ; puts Dir.entries("#{parent_directory}/pages/common") ; puts Dir.entries("#{parent_directory}/pages/windows") ; puts Dir.entries("#{parent_directory}/pages/osx") ; puts Dir.entries("#{parent_directory}/pages/sunos") ; exit }
+
+  opt.on("--update", "-u", "Updates the page cache") { update_pages_opt(parent_directory) ; exit}
 end.parse!
