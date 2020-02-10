27ba0ff260f47986fb98a5a31d6ba0081d5734ec
Marvin Johanning <manimusmuellus@googlemail.com>
2020-02-10 16:47:11 +0100

    Update --help flag formatting

--- lib/bin/option_parser.rb
+++ lib/bin/option_parser.rb
@@ -26,17 +26,21 @@ along with RubyTldr.  If not, see <http://www.gnu.org/licenses/>.
 parent_directory = File.expand_path('..', File.dirname(__FILE__))
 OptionParser.new do |opt|
 
+  #The default 'banner' that gets displayed when opening running rtldr --help
   opt.banner =
+    "RubyTldr".bold() + " (rtldr) is a program that will show you man-pages that have been condensed down to the most important information and are thus much easier to read and comprehend.\n" \
+    "If you wish to see the tldr-page of a particular command / program, simply write 'rtldr' followed by the command you wish to see the tldr-page of (for example, " + "rtldr tar".bold() + ").\n\n" \
     "Usage: ".bold() + "rtldr".underline() + " [options]" + "\n"\
-    "RubyTldr (rtldr) is a program that will show you man-pages that have been condensed down to the most important information and are thus much easier to read and comprehend.\n" \
-    "If you wish to see the tldr-page of a particular command / program, simply write 'rtldr' followed by the command you wish to see the tldr-page of (for example, " + "rtldr tar".bold() + ")"
 
+  #Printing the help information of every command
   opt.on("--help", "-h", "Prints this help") do
     puts opt
     exit
   end
 
+  #Listing every available command
   opt.on("--list", "-l", "Lists all the available commands") { puts Dir.entries("#{parent_directory}/pages/linux") ; puts Dir.entries("#{parent_directory}/pages/common") ; puts Dir.entries("#{parent_directory}/pages/windows") ; puts Dir.entries("#{parent_directory}/pages/osx") ; puts Dir.entries("#{parent_directory}/pages/sunos") ; exit }
 
+  #Updating the pages
   opt.on("--update", "-u", "Updates the page cache") { update_pages_opt(parent_directory) ; exit}
 end.parse!
