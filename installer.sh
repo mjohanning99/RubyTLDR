48abc77ab918fa26e62ae6fea35719f08a44bef6
Marvin Johanning <info@marvinjohanning.de>
2020-03-11 15:15:02 +0100

    Auto rtldr update on installation

--- installer.sh
+++ installer.sh
@@ -205,9 +205,11 @@ if [ -L /bin/rtldr ]; then echo "Symlink exists, not creating a new one."; else 
 
 printf "\n"
 
+/bin/rtldr --update
+
+echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'rtldr tldr'. If you get an explanation of this program, then everything worked correctly."
+
 if [[ -e installer.sh ]]; then
 	echo "Removing installer ..."
 	sudo rm installer.sh
 fi
-
-echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'rtldr tldr'. If you get an explanation of this program, then everything worked correctly."
